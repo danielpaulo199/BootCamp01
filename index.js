149a7d5e019134e16c838fc4a4ae1ef5aae27f8c
Daniel Paulo <Danielpaulo199@gmail.com>
2020-04-20 14:10:58 -0300

    Adicionado métodos put,post,delete

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
+
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+const users = ['Daniel', 'Jose', 'Marcos'];
 
-server.get('/teste', (req, res) => {
-    res.json({"message": "bomba"});
-} )
 
+
+server.get('/users', (req, res) => {
+    res.json(users);
+})
+
+server.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+    res.json(users[index]);
+} );
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+    users.push(name);
+
+    return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+    const { name } = req.body;
+    const { index } = req.params;
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+    
+    users.splice(index, 1);
+
+    return res.send(`Deletado user id: ${index}`);
+})
 
 server.listen('3000');
