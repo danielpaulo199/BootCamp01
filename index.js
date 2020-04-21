d7317944837a889022ef86ea8aa0c68a77fe0ff5
Daniel Paulo <danielpaulo199@gmail.com>
2020-04-20 22:37:13 -0300

    Add Middlewares to /users

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-
 const express = require('express');
-
 const server = express();
 server.use(express.json());
+
+server.use((req, res, next) => {
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+    return next();
+});
+
+// Middlewares
+function checkUserExists(req, res, next) {
+    if (!req.body.name){
+        return res.status(400).json({
+            error: 'User name required!'
+        });
+    }
+    return next();
+}
+
+function checkUserInArray (req, res, next){
+    const user = users[req.params.index];
+
+    if (!user) {
+        return res.status(400).json({
+            error : 'User Id not exists!',
+        });
+    }
+    req.user = user;
+    return next();
+}
+
+ // CRUD
 const users = ['Daniel', 'Jose', 'Marcos'];
 
-
-
-server.get('/users', (req, res) => {
+server.get('/users', checkUserInArray, (req, res) => { // Lista todos user
     res.json(users);
 })
 
-server.get('/users/:index', (req, res) => {
+server.get('/users/:index', checkUserInArray, (req, res) => {  // Lista user
     const { index } = req.params;
-    res.json(users[index]);
+    res.json(req.user);
 } );
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => { // add user
     const { name } = req.body;
     users.push(name);
 
     return res.json(users);
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res) => {  //update user
     const { name } = req.body;
     const { index } = req.params;
     users[index] = name;
@@ -31,7 +57,7 @@ server.put('/users/:index', (req, res) => {
     return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => { // delete user
     const { index } = req.params;
     
     users.splice(index, 1);
